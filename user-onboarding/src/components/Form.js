e4d769c6d822ec0b10badd66741805b2e60d67a9
harvey magana <heyharv2012@gmail.com>
2020-08-11 18:58:02 -0700

    Starting the project by clearing out the scaffold cold and adding mine.

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+const [ formState, setFormState ] = useState({
+    name: '', 
+    email: '', 
+    password: '', 
+})
+
+const Form = () => {
+    return (
+      <div className="App">
+          I am the Form component.
+      </div>
+    );
+  }
+
+export default Form
