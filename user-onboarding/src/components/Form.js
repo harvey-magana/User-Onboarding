849d90d963c8d48e70b612bb6927d6410ff93538
harvey magana <heyharv2012@gmail.com>
2020-08-11 19:50:52 -0700

    Added formSchema for form validateion.

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,6 +1,45 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import * as yup from 'yup';
 import '../App.css';
+
+const FormContainer = styled.form`
+	color: #2196f3;
+    display: block;
+	width: 300px;
+	margin: 50px auto;
+`;
+
+const FormGroup = styled.div`
+	color: #2196f3;
+    display: block;
+	width: 300px;
+	margin: 50px auto;
+`;
+
+const Label = styled.label`
+	margin-bottom: 0.5em;
+	color: #2196f3;
+    display: block;
+`;
+
+const Input = styled.input`
+	padding: 0.5em;
+	color: #2196f3;
+	background: papayawhip;
+	border: none;
+	border-radius: 3px;
+	width: 100%;
+	margin-bottom: 0.5em;
+`;
+
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #2196f3;
+  border-radius: 3px;
+`;
 
 const Form = () => {
     const [ formState, setFormState ] = useState({
@@ -25,8 +64,8 @@ const Form = () => {
                 setErrors({
                     ...errors, 
                     [e.target.name]: ""
-                });
-            });
+                })
+            })
             .catch((err) => {
                 console.log(err);
                 setErrors({
@@ -50,19 +89,25 @@ const Form = () => {
         console.log("form submitted!");
     }
 
+    const formSchema = yup.object().shape({
+        name: yup.string().required("Name is required"), 
+        email: yup.string().email("Make sure you enter a valid email").required("Must include an email"), 
+        password: yup.string().required('Password is required')
+    })
+
     return (
       <div className="App">
-        <Form onSubmit={formSubmit}>
+        <FormContainer onSubmit={formSubmit}>
             <FormGroup>
                 <Label htmlFor="label">Name:</Label>
                 <Input id="label" name="name" onChange={inputChange} />
                 <Label htmlFor="label">Email:</Label>
                 <Input id="label" name="email" onChange={inputChange} />
-                <Label htmlFor="label">Role:</Label>
-                <Input id="label" name="role" onChange={inputChange} />
+                <Label htmlFor="label">Password:</Label>
+                <Input id="label" name="password" onChange={inputChange} />
                 <Button primary>Primary</Button>
             </FormGroup>
-        </Form>
+        </FormContainer>
       </div>
     );
   }
