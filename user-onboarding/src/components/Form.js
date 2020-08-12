b66e2a0d52931d58114d3eb9a546c30be7eda2eb
harvey magana <heyharv2012@gmail.com>
2020-08-11 19:22:45 -0700

    Inputchange method added.

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
 import '../App.css';
 
-const [ formState, setFormState ] = useState({
-    name: '', 
-    email: '', 
-    password: '', 
-});
-
-const [ buttonDisabled, setButtonDisabled ] = useState(true);
-
-const [ errors, setErrors ] = useState({
-    name: '', 
-    email: '', 
-    password: '', 
-});
-
 const Form = () => {
+    const [ formState, setFormState ] = useState({
+        name: '', 
+        email: '', 
+        password: '' 
+    })
+
+    const [ buttonDisabled, setButtonDisabled ] = useState(true);
+
+    const [ errors, setErrors ] = useState({
+        name: '', 
+        email: '', 
+        password: '', 
+    });
+
+    
+    const inputChange = (e) => {
+        const newFormData = {
+            ...formState, 
+            [e.target.name]: e.target.value
+        }
+        validateChange(e);
+        setFormState(newFormData);
+    }
+
     return (
       <div className="App">
           I am the Form component.
